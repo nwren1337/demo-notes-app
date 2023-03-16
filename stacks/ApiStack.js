1b97d95a904515ccd03a73648fb229443e6c3e59
Nate Wren <nwren92@gmail.com>
2023-03-15 22:13:53 -0400

    added api for adding and getting note

---
+++ stacks/ApiStack.js
@@ -0,0 +1,29 @@
+import { Api, use } from "sst/constructs";
+import { StorageStack } from "./StorageStack";
+
+export function ApiStack({ stack, app }) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, "Api", {
+    defaults: {
+      function: {
+        bind: [table],
+      },
+    },
+    routes: {
+      "POST /notes": "packages/functions/src/create.main",
+      "GET /notes/{id}": "packages/functions/src/get.main",
+    },
+  });
+
+  // Show the API endpoint in the output
+  stack.addOutputs({
+    ApiEndpoint: api.url,
+  });
+
+  // Return the API resource
+  return {
+    api,
+  };
+}

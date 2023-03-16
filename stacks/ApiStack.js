0a3927b3a66ec5744703c36147ce7db60eedc4f7
Nate Wren <nwren92@gmail.com>
2023-03-15 22:45:51 -0400

    added update and delete

--- stacks/ApiStack.js
+++ stacks/ApiStack.js
@@ -15,6 +15,8 @@ export function ApiStack({ stack, app }) {
       "POST /notes": "packages/functions/src/create.main",
       "GET /notes/{id}": "packages/functions/src/get.main",
       "GET /notes": "packages/functions/src/list.main",
+      "PUT /notes/{id}": "packages/functions/src/update.main",
+      "DELETE /notes/{id}": "packages/functions/src/delete.main",
     },
   });
 
